9571d77e4992f969201708f9914c38803a2c4f01
Bleuette <102277517+BleuHere@users.noreply.github.com>
2022-11-03 16:17:31 +1300

    more formatting, css

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -2,7 +2,7 @@ import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
   return (
-    <main>
+    <main className="short">
       <MetaTags title="About" description="About page" />
 
       <h1>About Me</h1>
