07dda38135a54a6eb70f55b9932657f109be18e8
Bleuette <102277517+BleuHere@users.noreply.github.com>
2022-11-03 16:21:57 +1300

    add Canterbury Tech link

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -2,7 +2,7 @@ import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
   return (
-    <main className="short">
+    <main>
       <MetaTags title="About" description="About page" />
 
       <h1>About Me</h1>
